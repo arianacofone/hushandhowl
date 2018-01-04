ea0f3254f9669fdc6dfa7a935af2ebbd80b7bd8f
Ariana <ariana@Arianas-Air.fios-router.home>
2018-01-04 08:05:54 -0500

    add registration feature

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import { Registration } from './components/registration/Registration.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      email: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    alert(this.state.email + ', thanks for joining Hush/Howl!');
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2>HUSH/HOWL</h2>
+        <h4>AN EXPERIMENT IN THE EVERYDAY SUPRISES</h4>
+        <form onSubmit={this.handleSubmit} id="form">
+          <input
+            className="input"
+            type="email"
+            placeholder="EMAIL"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <input
+            className="input"
+            id="sendButton"
+            type="submit"
+            value="join the club"
+            onClick={this.handleSubmit}
+          />
+        </form>
       </div>
     );
   }
---
+++ src/components/registration/Registration.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Registration.css';
+
+class Registration extends Component {
+  render() {
+    return (
+      <div>
+        <h2>TESTING</h2>
+       </div>
+    );
+  }
+}
+
+export default Registration;
